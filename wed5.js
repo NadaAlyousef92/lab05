9548a879fa6c07ac025852a98124a8e7e3ccde9b
NadaAlyousef92 <nada.alyousef92@gmail.com>
2021-01-31 22:54:51 +0200

    lab5

---
+++ wed5.js
@@ -0,0 +1,45 @@
+
+
+
+var order;
+
+while (order !== "wedding" && order == "reception") {
+    order = prompt("what is your celebration wedding or reception?");
+}
+
+
+
+
+var order = prompt('what do you have wedding or reception?');
+
+while (order !== " reception " && order == " wedding") {
+    order = prompt("what do you have wedding or reception ?");
+
+}
+
+var itemorder = "";
+
+if (order === "wedding") {
+     <img scr='https://lh3.googleusercontent.com/proxy/KxtFgvyES7DSgKVTCG9lQTPNaNXzsiXw9UJAUDcgp-L9QojV_8p6FhyCab7QM5tHkPZCrGYBMQfPs61Hiq83Itd22N61t3tvxtNxkvXy9u3Y02dcpQ'/>
+
+    // addd image
+}else if (order === "reception") {
+<img scr="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.3a2ilati.com%2Farticle%2F%25D8%25B5%25D9%2588%25D8%25B1-%25D9%2588%25D8%25A7%25D9%2581%25D9%2583%25D8%25A7%25D8%25B1-%25D8%25B7%25D8%25A7%25D9%2588%25D9%2584%25D8%25A7%25D8%25AA-%25D8%25A7%25D8%25B3%25D8%25AA%25D9%2582%25D8%25A8%25D8%25A7%25D9%2584-%25D9%2585%25D9%2588%25D8%25A7%25D9%2584%25D9%258A%25D8%25AF-1230819&psig=AOvVaw13_EXxPS1q28AyWJzYifjZ&ust=1612191203966000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCPiSkbK2xu4CFQAAAAAdAAAAABAZ"/>;
+
+}
+var nooforder = prompt("how many the invitees ?");
+
+var result;
+for (var i = 0; i < nooforder; i++); {
+    result = result+itemoreder;
+    
+}
+
+
+
+document.writeresult;
+document.getElementById("order").innerText.order;
+document.getElementById("no of order").innerText.onorder;
+
+confirm(" are you happy of our services ? ");
+alert(" Thank you for your selections ");
